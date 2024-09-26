a2d05111fae2d902c3d876fe5ddf73a5cfc82b1c
mina bergova <bergovam@gmail.com>
2024-09-26 17:39:14 +0300

    Add create movie page

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ JS Back-End workshop @SoftUni
     5. Setup static route
     6. Render home page
     7. Add routes and controllers
-    8, Extract partial template
+    8. Extract partial template
+    9. Add service and data layers
+    10. Add Create page
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import homeController from "./controllers/homeController.js";
+import movieController from "./controllers/movieController.js";
 
 const router = Router();
 
 router.use(homeController);
+router.use('/movies', movieController)
 
 export default router;
